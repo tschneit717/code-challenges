5b2930871059c60a78461adf2f4e10d1d3c0ed19
tschneit717 <tschneit717@gmail.com>
2022-12-13 11:21:53 -0500

    Linked lists

--- add-two-numbers/solution.js
+++ add-two-numbers/solution.js
@@ -11,67 +11,53 @@
  * @return {ListNode}
  */
 
+
 const samples = [
-  [(l1 = [2, 4, 3]), (l2 = [5, 6, 4])],
-  [(l1 = [0]), (l2 = [0])],
-  [(l1 = [9, 9, 9, 9, 9, 9, 9]), (l2 = [9, 9, 9, 9])],
+  [l1 = [2, 4, 3], l2 = [5, 6, 4]],
+  [l1 = [0], l2 = [0]],
+  [l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9]],
 ];
 
-var addTwoNumbers = function (l1, l2) {
-  const listNode = (val, next) => {
-    return {
-      val: val === undefined ? 0 : val,
-      next: next === undefined ? null : next,
-    };
-  };
 
-  const linkedList = (head = null) => {
-    return { head: head };
-  };
-
-  const createList = (list) => {
-    let size = 0;
-    let node;
-    let count = list.length - 1;
-    let linked = linkedList(listNode(list[count]));
-
-    let current = linked.head;
-    while (count > 0) {
-      count--;
-      node = listNode(list[count]);
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = node;
-      size++;
-    }
-    return { linked, size };
-  };
-
-  let list1 = createList(l1);
-  list2 = createList(l2);
-  let count = 0;
-  let current1 = list1.linked.head;
-  let current2 = list2.linked.head;
-  let activeValue1 = [];
-  let activeValue2 = [];
-  while (list2.size >= count || list1.size >= count) {
-    if (current1) {
-      activeValue1.push(current1.val);
-      current1 = current1.next;
-    }
-    if (current2) {
-      activeValue2.push(current2.val);
-      current2 = current2.next;
-    }
-
-    count++;
+class ListNode {
+  constructor(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
   }
-  const result =
-    parseInt(activeValue1.join('')) + parseInt(activeValue2.join(''));
-  createList(result.split(''));
-};
+}
+
+const createSamples = (sample) => {
+  return sample.map(item => {
+    return item.reduceRight((next, val) => ({ val, next }), null);
+  })
+}
+
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let prevNode = new ListNode()
+  const head = prevNode;
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let val1 = 0;
+    let val2 = 0;
+    if (l1 !== null) {
+      val1 = l1.val
+      l1 = l1.next
+    }
+    if (l2 !== null) {
+      val2 = l2.val
+      l2 = l2.next
+    }
+    const sum = val1 + val2 + carry;
+    carry = sum > 9 ? 1 : 0;
+    const digit = sum % 10;
+    const currentNode = new ListNode(digit)
+    prevNode.next = currentNode;
+    prevNode = currentNode;
+  };
+  return head.next
+}
 
 samples.forEach((sample) => {
-  addTwoNumbers(sample[0], sample[1]);
+  const tests = createSamples(sample);
+  console.log(addTwoNumbers(tests[0], tests[1]));
 });
---
+++ linked-lists/linked-list-tutorial.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node('A')
+const b = new Node('B')
+const c = new Node('C')
+const d = new Node('D')
+const e = new Node('E')
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+///  A -> B -> C -> D -> E -> NULL
+/// cur
+
+const printLinkedList = (head) => {
+  if (head === null) return;
+  console.log(head.val);
+  printLinkedList(head.next);
+}
+
+const conditionalCheck = (val, check) => {
+  return val === check
+}
+
+const traverseList = (head, conditional, callback) => {
+  if (head === null) return;
+  if (conditionalCheck(head.val, conditional)) {
+    callback(head)
+  } else {
+    traverseList(head.next);
+  }
+}
+
+
+traverseList(a, 'B', callback => console.log(callback))
+
+
