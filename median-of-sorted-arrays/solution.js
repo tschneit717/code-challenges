f876d5b73cd0d40703a100c62f78115795610b21
Thomas Schneider <tschneit717@gmail.com>
2023-01-10 20:51:56 -0500

    Updated using bubble sort

--- median-of-sorted-arrays/solution.js
+++ median-of-sorted-arrays/solution.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  let combined = nums1.concat(nums2).sort((a, b) => a - b);
+  let combined = nums1.concat(nums2);
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < combined.length; i++) {
+      if (combined[i - 1] > combined[i]) {
+        done = false;
+        let temp = combined[i - 1];
+        combined[i - 1] = combined[i];
+        combined[i] = temp;
+      }
+    }
+  }
   if (combined.length % 2 == 0) {
     return (
       (combined[Math.floor(combined.length / 2)] +
