87285335bd4dde635d6a963d2d7b8c4a19511b49
Thomas Schneider <tschneit717@gmail.com>
2023-01-22 16:29:36 -0500

    Solution for height of a binary tree

---
+++ height-of-binary-tree/solution.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == null) return 0
+  let depthLeft = maxDepth(root.left)
+  let depthRight = maxDepth(root.right)
+  if (depthLeft > depthRight) {
+      return (depthLeft + 1)
+  } else {
+      return (depthRight + 1)
+  }
+
+};
